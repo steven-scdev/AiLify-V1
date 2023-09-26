38dadd2ef9b532c6a2b620204233586c35de91ad
Yujun Chen <steven.yujunchen@berkeley.edu>
2023-09-26 16:39:07 -0700

    Ready for production

--- components/AiLifyContainer.tsx
+++ components/AiLifyContainer.tsx
@@ -8,28 +8,32 @@ const AiLifyContainer: NextPage = () => {
           <div className="self-stretch h-36 flex flex-col items-start justify-start">
             <div className="self-stretch flex flex-col items-start justify-start py-0 pr-[901px] pl-0">
               <h1 className="m-0 relative text-inherit leading-[72px] font-bold font-inherit inline-block max-w-[1336px]">
-                New
+                AiLify
               </h1>
             </div>
             <div className="self-stretch" />
           </div>
           <div className="self-stretch overflow-hidden flex flex-col items-start justify-start gap-[1px] text-lg font-inter">
-            <div className="w-full flex flex-col items-start justify-start py-0 pr-4 pl-0 box-border max-w-[380px]">
-              <div className="w-full relative leading-[28px] inline-block max-w-[380px]">
-                <p className="m-0">Empower Your Business with Cutting-Edge</p>
-                <p className="m-0">AI Solutions</p>
+            <div className="w-full flex flex-col items-start justify-start py-0 pb-8pr-4 pl-0 box-border max-w-[800px]">
+              <div className="w-full relative leading-[28px] inline-block max-w-[800px] ">
+                <p className="m-0 font-bold text-5xl">
+                  Customized AI Agent that double your sales
+                </p>
+                <p className="m-0 text-4xl decoration-gray-300">
+                  By Talking Less
+                </p>
               </div>
             </div>
-            <div className="self-stretch h-[88px] flex flex-row items-start justify-start pt-5 pb-0 pr-[1030.93994140625px] pl-0 box-border gap-[12px]">
+            <div className="self-stretch h-[88px] flex flex-row items-start justify-start  mt-5 pr-[1030.93994140625px] pl-0 box-border gap-[12px]">
               <div className="self-stretch flex flex-col items-start justify-start pt-0 px-0 pb-0.5">
-                <button className="cursor-pointer [border:none] py-3 pr-[31.031455993652344px] pl-[31.858543395996094px] bg-darkorange self-stretch rounded-lg flex flex-row items-start justify-start">
+                <button className="cursor-pointer [border:none] pr-[31.031455993652344px] pl-[31.858543395996094px] bg-darkorange self-stretch rounded-lg flex flex-row items-start justify-start">
                   <div className="relative text-base leading-[24px] font-medium font-inter text-white text-center">
                     Let's talk
                   </div>
                 </button>
               </div>
               <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch flex flex-col items-start justify-start">
-                <div className="self-stretch rounded-lg bg-white shadow-[0px_1px_2px_rgba(16,_24,_40,_0.05)] flex flex-row items-start justify-start py-[13px] pr-[31.322864532470703px] pl-[32.84713363647461px] border-[1px] border-solid border-gray-100">
+                <div className="self-stretch rounded-lg bg-white shadow-[0px_1px_2px_rgba(16,_24,_40,_0.05)] flex flex-row items-start justify-start  pr-[31.322864532470703px] pl-[32.84713363647461px] border-[1px] border-solid border-gray-100">
                   <div className="relative text-base leading-[24px] font-medium font-inter text-gray-100 text-center">
                     Our services
                   </div>
